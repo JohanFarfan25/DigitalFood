99efad9f3de35a4dd8a8e6da69cb279d355a6152
Johan Farfan <johanfarfan25@gmail.com>
2025-02-18 08:30:13 -0500

    Rechazadas

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -82,7 +82,7 @@
         <div class="row">
           <div class="col-8">
             <div class="numbers">
-              <p class="text-sm mb-0 text-capitalize font-weight-bold">Rechazado</p>
+              <p class="text-sm mb-0 text-capitalize font-weight-bold">Rechazadas</p>
               <h5 class="font-weight-bolder mb-0">
                 ${{$declined}}
                 <span class="text-success text-sm font-weight-bolder"></span>
